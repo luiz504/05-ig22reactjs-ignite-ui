ebe7cc699ef9621712ad7981c09fc59d05afcb9b
Luiz <l.bueno@xsensors.ai>
2023-05-22 11:17:13 -0300

    config stiches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,15 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$lg',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}> Hello Word</h1>
+  return (
+    <>
+      <Button>Hello word</Button>
+    </>
+  )
 }
---
+++ packages/react/styles/index.ts
@@ -0,0 +1,40 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    width: 'space',
+    minWidth: 'space',
+    maxWidth: 'space',
+    height: 'space',
+    minHeight: 'space',
+    maxHeight: 'space',
+  },
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
