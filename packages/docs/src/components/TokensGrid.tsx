a12e3246378c1d93dc55fc2fc5f986cbe22ed63d
Luiz <l.bueno@xsensors.ai>
2023-05-22 16:18:43 -0300

    docs: doing the rest of tokens

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,29 @@
+import '../styles/tokens-grid.css'
+
+interface TokenGridProps {
+  tokens: Record<string, string>
+  hasRemValue?: boolean
+}
+export function TokensGrid({ tokens, hasRemValue = false }: TokenGridProps) {
+  return (
+    <table className="tokens-grid">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+          {hasRemValue && <th>Pixels</th>}
+        </tr>
+      </thead>
+
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{value}</td>
+            {hasRemValue && <th>{Number(value.replace('rem', '')) * 16}px</th>}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
