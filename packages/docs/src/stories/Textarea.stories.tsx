d36c668941ebc4f428c74c10ba30bd0c38ae20c2
Luiz <l.bueno@xsensors.ai>
2023-05-25 07:56:15 -0300

    style: textarea component

---
+++ packages/docs/src/stories/Textarea.stories.tsx
@@ -0,0 +1,34 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Textarea, TextareaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Textarea',
+  component: Textarea,
+  tags: ['autodocs'],
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text>Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextareaProps>
+
+export const Primary: StoryObj<TextareaProps> = {
+  args: {
+    placeholder: 'Add any observation...',
+  },
+}
+
+export const Disabled: StoryObj<TextareaProps> = {
+  args: {
+    disabled: true,
+  },
+}
