435c0ce17ce0ee8617e4ba5ff1f084370ef5cadf
Luiz <l.bueno@xsensors.ai>
2023-05-25 08:18:21 -0300

    style: checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,30 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text>Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true,
+  },
+}
